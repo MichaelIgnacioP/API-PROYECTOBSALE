82cf2a6bbc22e3b2b199f2dec8f176ef36dccee4
MichaelPadilla <michaelignaciop@gmail.com>
2023-05-26 06:17:28 -0400

    edit environ

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use('/contact', contactNotification);
 // Manejo de CORS para todas las rutas
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
