c4f06571679241d68e085b725a7f98d89ab25ae3
MichaelPadilla <michaelignaciop@gmail.com>
2023-05-26 08:39:41 -0400

    refacto index new

--- index.js
+++ index.js
@@ -17,18 +17,40 @@ app.use('/api', cors());
 app.use(bodyParser.json());
 
 // Rutas de inicio de sesión y registro
-app.use('/login', loginRoutes);
-app.use('/register', registerRoutes);
-app.use('/landingpage', landingPageRoutes);
-app.use('/contact', contactNotification);
+// app.use('/login', loginRoutes);
+// app.use('/register', registerRoutes);
+// app.use('/landingpage', landingPageRoutes);
+// app.use('/contact', contactNotification);
+
 
 // Manejo de CORS para todas las rutas
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+//   });
+
+  // Configuración de CORS
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+app.use('/login', allowCors(loginRoutes));
+app.use('/register', allowCors(registerRoutes));
+app.use('/landingpage', allowCors(landingPageRoutes));
+app.use('/contact', allowCors(contactNotification));
 
 // Iniciar el servidor
 const port = process.env.PORT;
